839a81e248b3efbf30d6a7fe6aaf9b4069829eca
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-03-17 17:07:48 +1000

    feat: implement converter

---
+++ currency-converter/converter.js
@@ -0,0 +1,13 @@
+const RATES = {
+  USD: 0.013,
+  EUR: 0.012,
+  THB: 0.45,
+};
+
+function convert(RUB, currency) {
+  if(!RATES[currency]) {
+    return null;
+  }
+
+  return RUB * RATES[currency];
+}

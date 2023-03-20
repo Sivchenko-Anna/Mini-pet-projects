bab3899207eb45ad7826f2ab32a7a8c836c077a8
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-03-20 20:18:46 +1000

    feat: add calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,30 @@
+const OPERATIONS = {
+  sum: "+",
+  subtraction: "-",
+  multiply: "*",
+  division: '/'
+};
+
+function calculator ({a, b, operation}) {
+  let result = null;
+
+  switch (operation) {
+    case OPERATIONS.sum:
+      result = sum(a, b);
+      break;
+
+    case OPERATIONS.subtraction:
+      result = subtraction(a, b);
+      break;
+
+    case OPERATIONS.multiply:
+      result = multiply(a, b);
+      break;
+
+    case OPERATIONS.division:
+      result = division(a, b);
+      break;
+
+    default: break;
+  }
+}

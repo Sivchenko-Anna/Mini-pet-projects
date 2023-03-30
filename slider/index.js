fdcf7364a065a7be1254a5370be03ac7b64a0ab7
Sivchenko-Anna <anya.malakhova.99@mail.ru>
2023-03-30 11:32:08 +1000

    feat: implemented slider switching by buttons at the bottom

--- slider/index.js
+++ slider/index.js
@@ -2,6 +2,7 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
 const images = document.querySelectorAll(".image");
+const bottom = document.querySelector(".bottom");
 
 let slideNumber = 1;
 const length = images.length;
@@ -28,8 +29,39 @@ const getLastSlide = () => {
 
 right.addEventListener("click", () => {
   slideNumber < length ? nextSlide() : getFirstSlide();
+  changeColor();
 });
 
 left.addEventListener("click", () => {
   slideNumber > 1 ? prevSlide() : getLastSlide();
+  changeColor();
 })
+
+for (let i = 0; i < length; i++) {
+  const div = document.createElement("div");
+  div.className = "button";
+  bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll(".button");
+buttons[0].style.backgroundColor = "rgb(33 33 33)";
+
+const resetBg = () => {
+  buttons.forEach(button => {
+    button.style.backgroundColor = "transparent";
+  })
+}
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    resetBg();
+    button.style.backgroundColor = "rgb(33 33 33)";
+    slideNumber = index + 1;
+    slider.style.transform = `translateX(-${index * 800}px)`;
+  })
+})
+
+const changeColor = () => {
+  resetBg();
+  buttons[slideNumber - 1].style.backgroundColor = "rgb(33 33 33)";
+};
